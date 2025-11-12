dd914c6c6c0f8a8d5a64d09b2e8896998a4e212d
codeBuddha <jamwujustyle@gmail.com>
2025-11-12 22:54:27 +0500

    cetralize footer. draft styling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("/header.html")
+  const isPages = window.location.pathname.includes("/pages/");
+  const baseUrl = isPages ? ".." : ".";
+
+  fetch(`${baseUrl}/header.html`)
     .then((response) => response.text())
     .then((data) => {
       document.body.insertAdjacentHTML("afterbegin", data);
+    });
+
+  fetch(`${baseUrl}/footer.html`)
+    .then((response) => response.text())
+    .then((data) => {
+      document.body.insertAdjacentHTML("beforeend", data);
     });
 });
